dc813b742c859824a0b1d527a5db173bf4a28836
fermelli <luisfernandosalgadomiguez@gmail.com>
2024-01-12 01:05:23 -0400

    interceptor movido a service

--- resources/js/src/services/auth.js
+++ resources/js/src/services/auth.js
@@ -1,39 +1,4 @@
 import service from "./service";
-import { useAuthStore } from "@/stores/auth";
-import { storeToRefs } from "pinia";
-import {
-    errorNetworkCode,
-    pageExpiredCode,
-    unauthorizedCode,
-} from "@/utils/constants";
-import { useToast } from "vue-toastification";
-
-service.interceptors.response.use(
-    (response) => response,
-    async (error) => {
-        const authStore = useAuthStore();
-        const { setUser } = authStore;
-        const { userGetter } = storeToRefs(authStore);
-        const toast = useToast();
-
-        if (error.code === errorNetworkCode) {
-            toast.error(error.message);
-        }
-
-        if (
-            error.response &&
-            [unauthorizedCode, pageExpiredCode].includes(error.response.status)
-        ) {
-            toast.error(error.response.data.message);
-
-            if (userGetter.value) {
-                setUser(null);
-            }
-        }
-
-        return Promise.reject(error);
-    },
-);
 
 async function login(credentials) {
     await service.get("/sanctum/csrf-cookie");
--- resources/js/src/services/service.js
+++ resources/js/src/services/service.js
@@ -19,4 +19,40 @@ const service = axios.create({
     },
 });
 
+import { useAuthStore } from "@/stores/auth";
+import { storeToRefs } from "pinia";
+import {
+    errorNetworkCode,
+    pageExpiredCode,
+    unauthorizedCode,
+} from "@/utils/constants";
+import { useToast } from "vue-toastification";
+
+service.interceptors.response.use(
+    (response) => response,
+    async (error) => {
+        const authStore = useAuthStore();
+        const { setUser } = authStore;
+        const { userGetter } = storeToRefs(authStore);
+        const toast = useToast();
+
+        if (error.code === errorNetworkCode) {
+            toast.error(error.message);
+        }
+
+        if (
+            error.response &&
+            [unauthorizedCode, pageExpiredCode].includes(error.response.status)
+        ) {
+            toast.error(error.response.data.message);
+
+            if (userGetter.value) {
+                setUser(null);
+            }
+        }
+
+        return Promise.reject(error);
+    },
+);
+
 export default service;
